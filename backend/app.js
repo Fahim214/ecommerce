804704052e1d9a34156f451e98fc3f82dcc81a27
Fahim214 <fahimtu456@gmail.com>
2022-03-09 22:40:55 +0700

    Version 1.0.0

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,12 @@ const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 const fileUpload = require('express-fileupload')
+const path = require('path')
 
 const errorMidleware = require('./middleware/error')
+
+
+if (process.env.NODE_ENV !== 'PRODUCTION') require('dotenv').config({ path: 'backend/config/config.env' })
 
 
 app.use(cors())
@@ -21,6 +25,18 @@ const auth = require('./routes/auth')
 
 app.use('/api/v1', products)
 app.use('/api/v1', auth)
+
+
+
+if (process.env.NODE_ENV === 'PRODUCTION') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
+    })
+}
+
+
 
 // Middleware handle error
 app.use(errorMidleware)
