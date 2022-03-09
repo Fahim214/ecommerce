adc50b1959d15b10a5b28b3e87e81adad2fbdaa1
Fahim214 <fahimtu456@gmail.com>
2022-03-09 14:55:16 +0700

    update search

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useState, useEffect } from "react";
-import Pagination from 'react-js-pagination'
+import Pagination from "react-js-pagination";
 import MetaData from "./layout/MetaData";
 import Product from "./product/Product";
 import Loader from "./layout/Loader";
@@ -7,72 +7,98 @@ import Loader from "./layout/Loader";
 import { useDispatch, useSelector } from "react-redux";
 import { getProducts } from "../actions/ProductAction";
 import { useAlert } from "react-alert";
-import SliderComp from './layout/SliderComp'
+import SliderComp from "./layout/SliderComp";
 import CategoryComp from "./layout/CategoryComp";
+import { Button, Col, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Home = () => {
+  const [currentPage, setCurrentPage] = useState(1);
 
-  const [currentPage, setCurrentPage] = useState(1)
-
-  const alert = useAlert()
+  const alert = useAlert();
   const dispatch = useDispatch();
 
-  const { loading, products, error, productsCount, resPerPage } = useSelector(state => state.products
+  const { loading, products, error, productsCount, resPerPage } = useSelector(
+    (state) => state.products
   );
 
   useEffect(() => {
-
-    if(error){
-      return alert.error(error)
+    if (error) {
+      return alert.error(error);
     }
 
     dispatch(getProducts(currentPage));
-
   }, [dispatch, alert, error, currentPage]);
 
   function setCurrentPageNo(pageNumber) {
-    setCurrentPage(pageNumber)
+    setCurrentPage(pageNumber);
   }
 
   return (
     <>
       <SliderComp />
       <CategoryComp />
-    <div className=" container-fluid" style={{width: "95%"}}>
-      {loading ? 
-        <Loader /> : (
-        <Fragment>
-          <MetaData title={"Buy Best Product Online"} />
-          <h1 id="products_heading">Latest product</h1>
-          <section id="products" className="container-fluid mt-5" style={{width: "100%"}}>
-            <div className="row">
-              {products &&
-                products.map((product) => (
-                  <Product key={product._id} product={product} />
-                ))}
-            </div>
-          </section>
-          
-                  
-            <div className="d-flex justify-content-center mt-5">
-            <Pagination 
-              activePage={currentPage}
-              itemsCountPerPage={resPerPage}
-              totalItemsCount={productsCount}
-              onChange={setCurrentPageNo}
-              nextPageText={'Next'}
-              prevPageText={'Prev'}
-              firstPageText={'First'}
-              lastPageText={'Last'}
-              itemClass='page-item'
-              linkClass='page-link'
-            />
-          </div>
-        
+      <div className=" container-fluid" style={{ width: "95%" }}>
+        {loading ? (
+          <Loader />
+        ) : (
+          <Fragment>
+            <MetaData title={"Buy Best Product Online"} />
+            <Row>
+              <Col md={6}>
+                <h1 id="products_heading">Latest product</h1>
+              </Col>
+              <Col md={6} style={{ marginTop: 40 }}>
+                <Link to="/" className="btn btn-outline-primary mx-1">
+                  Elektronik
+                </Link>
+                <Link to="/" className="btn btn-outline-primary mx-1">
+                  Buku
+                </Link>
+                <Link to="/" className="btn btn-outline-primary mx-1">
+                  Baju
+                </Link>
+                <Link to="/" className="btn btn-outline-primary mx-1">
+                  Celana
+                </Link>
+                <Link to="/" className="btn btn-outline-primary mx-1">
+                  Kaos
+                </Link>
+                <Link to="/" className="btn btn-outline-primary mx-1">
+                  Alat Rumah Tangga
+                </Link>
+              </Col>
+            </Row>
+            <section
+              id="products"
+              className="container-fluid mt-5"
+              style={{ width: "100%" }}
+            >
+              <div className="row">
+                {products &&
+                  products.map((product) => (
+                    <Product key={product._id} product={product} />
+                  ))}
+              </div>
+            </section>
 
-        </Fragment>
-      )}
-    </div>
+            <div className="d-flex justify-content-center mt-5">
+              <Pagination
+                activePage={currentPage}
+                itemsCountPerPage={resPerPage}
+                totalItemsCount={productsCount}
+                onChange={setCurrentPageNo}
+                nextPageText={"Next"}
+                prevPageText={"Prev"}
+                firstPageText={"First"}
+                lastPageText={"Last"}
+                itemClass="page-item"
+                linkClass="page-link"
+              />
+            </div>
+          </Fragment>
+        )}
+      </div>
     </>
   );
 };
--- frontend/src/components/layout/Search.js
+++ frontend/src/components/layout/Search.js
@@ -1,40 +1,37 @@
-// import React, { useState } from 'react'
-// import {useNavigate} from 'react-router-dom'
+import React, { useState } from 'react'
 
-// const Search = ({history}) => {
+const Search = ({ history }) => {
 
-//     const [keyword, setKeyword] = useState('')
+    const [keyword, setKeyword] = useState('');
 
-//     const searchHandle = (e) => {
-//         e.preventDefault()
-        
-//         if(keyword.trim()) {
-//             history.push(`/search/${keyword}`)
-//         } else {
-//             history.push('/')
-//         }
-//     }
-//   return (
-//     <div>
-//         <form onSubmit={searchHandle}>
-//         <div className="input-group">
-//             <input
-//               type="text"
-//               id="search_field"
-//               className="form-control"
-//               value={keyword}
-//               placeholder="Enter Product Name ..."
-//               onChange={(e) => setKeyword(e.target.value)}
-//             />
-//             <div className="input-group-append">
-//               <button id="search_btn" className="btn">
-//                 <i className="fa fa-search" aria-hidden="true"></i>
-//               </button>
-//             </div>
-//           </div>
-//         </form>
-//     </div>
-//   )
-// }
+    const searchHandler = (e) => {
+        e.preventDefault()
 
-// export default Search
+        if (keyword.trim()) {
+            history.push(`/search/${keyword}`)
+        } else {
+            history.push('/')
+        }
+    }
+
+    return (
+        <form onSubmit={searchHandler} >
+            <div className="input-group">
+                <input
+                    type="text"
+                    id="search_field"
+                    className="form-control"
+                    placeholder="Enter Product Name ..."
+                    onChange={(e) => setKeyword(e.target.value)}
+                />
+                <div className="input-group-append">
+                    <button id="search_btn" className="btn">
+                        <i className="fa fa-search" aria-hidden="true"></i>
+                    </button>
+                </div>
+            </div>
+        </form>
+    )
+}
+
+export default Search
